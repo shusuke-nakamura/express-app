60ee1fccc958e6cf846e46b36471e5986677b559
Shusuke Nakamura <shuchan.galaxy@gmail.com>
2020-04-25 04:05:46 +0900

    stylesheetの適用

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ var ejs = require('ejs');
 var app = express();
 
 app.engine('ejs', ejs.renderFile);
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
   // index.ejsをレンダリングする
+  var msg = "This is Express Page!<br>" + "これは、スタイルシートを利用した例です。";
   res.render('index.ejs', {
     title: 'Index',
-    content: 'This is Express-app Top page!',
+    content: msg,
   });
 });
 
