2e0047e12e3ca96b86fce7a796c06d49a1d23bf4
Shusuke Nakamura <shuchan.galaxy@gmail.com>
2020-04-25 03:50:14 +0900

    ejsのレンダリングを追加

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 var express = require('express');
+var ejs = require('ejs');
 var app = express();
 
+app.engine('ejs', ejs.renderFile);
+
 app.get('/', (req, res) => {
-  res.send('Welcome to Express!');
+  // index.ejsをレンダリングする
+  res.render('index.ejs', {
+    title: 'Index',
+    content: 'This is Express-app Top page!',
+  });
 });
 
 app.listen(3000, () => {
