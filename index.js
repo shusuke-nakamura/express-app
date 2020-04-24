22c74af932c219b98ad098ed6edf76c0aa1d2f63
Shusuke Nakamura <shuchan.galaxy@gmail.com>
2020-04-25 06:26:26 +0900

    P.209 学習完了

--- index.js
+++ index.js
@@ -5,12 +5,24 @@ var app = express();
 app.engine('ejs', ejs.renderFile);
 app.use(express.static('public'));
 
+// ※ トップページ
 app.get('/', (req, res) => {
   // index.ejsをレンダリングする
   var msg = "This is Express Page!<br>" + "これは、スタイルシートを利用した例です。";
   res.render('index.ejs', {
     title: 'Index',
     content: msg,
+    link:{href: '/other', text: '※別のページへ移動'},
+  });
+});
+
+// ※ otherページ
+app.get('/other', (req, res) => {
+  var msg = "This is Other Page!<br>" + "これは、用意された別のページです。";
+  res.render('index.ejs', {
+    title: 'Other',
+    content: msg,
+    link: {href: '/', text: '※トップに戻る'},
   });
 });
 
